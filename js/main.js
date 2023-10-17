c8460662ddf2ba95b42a6f7df66d30c464fe8fa5
dnegrin13 <dylannegrin13@gmail.com>
2023-10-17 16:12:22 +0200

    texte qui change en 500 mili seconde

--- js/main.js
+++ js/main.js
@@ -119,4 +119,118 @@ function division(nbra, nbrb) {
     }
   
 }
-console.log(division(nombreA,nombreB))
+console.log(division(nombre_A,nombre_B))
+
+//version naive
+
+function isEven(nbr) {
+    if ( nbr % 2 == 0) {
+        return true
+    }else {
+        return false
+    }
+}
+
+// version optimisé
+
+function isEven(nbr) {
+    return nbr % 2 == 0;
+}
+
+console.log(isEven(47))
+
+// changement de classe
+
+//GetElementsByClassName -> retourne toujour un tableau, même si il y a qu'un seul élément.
+//const red = document.getElementsByClassName('red') [0];
+const red = document.querySelector('.red');
+red.className = "blue"; // à éviter
+red.classList.add("gras");
+red.classList.remove("blue");
+red.classList.add("red");
+console.log(red)
+
+//EXERCICE
+// Faire une fonction 
+//replaceC(element_a_cibler, ancienne_class, nouvelle_classe)
+
+//replaceC(<a>, "btn-primary", "btn-danger")
+
+// <a href="#" class="btn btn-primary">Envoyer</a>
+
+//faire le coprs de la fonction, cette fonction ne retourne rien
+
+
+function replaceC(target, oldClass, nexClass) {
+    const t = document.querySelector(target);
+    if ( oldClass !== nexClass) {
+        if( t.classList.contains(oldClass)){
+            t.classList.remove(oldClass)
+            t.classList.add(newClass)
+        }else{
+            console.error("la classe n'existe pas dans l'élément " + target)
+        }
+    }else{
+        console.error("Vous voulez remplacer la class par elle même")
+    }
+}
+
+replaceC("#exo", "autruche", "green")
+
+/*
+    Créez une fonction qui : 
+        demande la saisie d'un rayon ;
+*/
+
+        // la surface d'un cercle est de pi * r2
+
+        function surface(r, isCircule) {
+            if (isCircule){
+                return Math.PI * r ** 2; 
+            }else {
+                return r ** 2; // c'est un carré
+            }
+           
+        }
+
+        console.log(surface(5))
+
+        //EVENEMENTS
+
+        const likes = document.getElementById('likes');
+        const btn = document.querySelector('.btn');
+        let likesNumber = 0
+
+        btn.addEventListener("click", function() {
+            likesNumber++;
+            likes.innerHTML = likesNumber;
+            this.classList.toggle("pink");
+            this.classList.toggle("chartreuse");
+        })
+
+        const body = document.querySelector('body');
+        const bouton = document.querySelector('.bouton')
+
+        bouton.addEventListener("click", function() {
+            body.classList.toggle("black");
+            body.classList.toggle("white");
+        })
+
+        // Timers
+
+        // Le setTimeout dans cette exemple au bout de 5 secondes (5000ms)
+        // la fonction anonyme sera traitée ( le console log envoyé)
+        setTimeout(function(){
+            console.log("coucou !!")
+        }, 5000)
+
+        setInterval(function(){
+            console.log("EXTERMINATE !!!")
+        }, 2000)
+
+        const lorem = document.getElementById('lorem')
+        
+        setInterval(function(){
+            lorem.classList.toggle("texte");
+            lorem.classList.toggle("textes");
+        }, 500)
