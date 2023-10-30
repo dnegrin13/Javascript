5c3987ba12cc76bf687a77295d739f687260a630
dnegrin13 <dylannegrin13@gmail.com>
2023-10-30 16:21:02 +0100

    interrogateur continuer

--- js/interrogateur.js
+++ js/interrogateur.js
@@ -3,12 +3,10 @@ const Interroger = document.getElementById('interroger')
 const btn = document.getElementById('btn')
 
 let nom = ["Dylan, Steven, Jonathan, Antonin, Johan, Gregory, Raphael, Clarisse, Jerome, Pierre"];
-let chiffe = ["1, 2, 3, 4, 5, 6, 7, 8, 9, 10"];
 
-function listeNom() {
-    
-}
-
-btn.addEventListener('click', function() {
-
-})
+nom.forEach(element => {
+    pInterroger.innerHTML += `<li>${nom}</li>`
+    btn.addEventListener('click', function() {
+        
+    })
+});
