2803c1be49c6e84a9bab15b5ab7653b83a29cada
dnegrin13 <dylannegrin13@gmail.com>
2023-10-31 16:03:02 +0100

    correction interrogateur

--- js/interrogateur.js
+++ js/interrogateur.js
@@ -1,12 +1,38 @@
+const nom = ["Dylan, Steven, Jonathan, Antonin, Johan, Gregory, Raphael, Clarisse, Jerome, Pierre"];
+
+let copyNom = [...nom];
+
 const pInterroger = document.getElementById('p-interroger')
-const Interroger = document.getElementById('interroger')
-const btn = document.getElementById('btn')
+const hangman = document.getElementById('hangman')
+const lastSacrified = document.getElementById('last-sacrified')
 
-let nom = ["Dylan, Steven, Jonathan, Antonin, Johan, Gregory, Raphael, Clarisse, Jerome, Pierre"];
-
-nom.forEach(element => {
+// Nous crééons la liste des personnes à sacrifier ici.
+function updateNom(){ 
+    pInterroger.innerHTML = '';
+    copyNom.forEach(element => {
     pInterroger.innerHTML += `<li>${nom}</li>`
-    btn.addEventListener('click', function() {
-        
-    })
-});
+    });
+}
+
+
+function genNumRand(){
+    return Math.floor(Math.random() * copyNom.length)
+}
+
+hangman.addEventListener('click', function() {
+    if ( copyNom.length === 0) {
+        copyNom = [...nom]
+        updateNom();
+    }
+    // On utilise la methode splice
+    // le premier argument est l'index de l'élément à enlever
+    // le deuxieme est nombre d'élément à enlever à partir de l'index
+    let rand = genNumRand()
+    lastSacrified.innerHTML = copyNom[rand];
+    copyNom.splice(rand, 1);
+    updateNom();
+   
+})
+
+// On update "GRAPHIQUEMENT" la liste pendant le premier chargement de la page
+updateNom();
